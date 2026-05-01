3ca002811536605c1c8721214a37428dfcd078ae
Mohamed Sobhy <mo.sobhy.z11@gmail.com>
2026-05-01 04:54:02 +0300

    UpdateProfileUi

--- lib/utiles/appAssets.dart
+++ lib/utiles/appAssets.dart
@@ -1,4 +1,13 @@
 class Appassets {
   static const String forgetPass = 'assets/images/Forget_Password.png';
   static const String mail_icon = 'assets/images/mail.png';
+  static const String avatar_1 = 'assets/images/avatar_1.png';
+  static const String avatar_2 = 'assets/images/avatar_2.png';
+  static const String avatar_3 = 'assets/images/avatar_3.png';
+  static const String avatar_4 = 'assets/images/avatar_4.png';
+  static const String avatar_5 = 'assets/images/avatar_5.png';
+  static const String avatar_6 = 'assets/images/avatar_6.png';
+  static const String avatar_7 = 'assets/images/avatar_7.png';
+  static const String avatar_8 = 'assets/images/avatar_8.png';
+  static const String avatar_9 = 'assets/images/avatar_9.png';
 }
