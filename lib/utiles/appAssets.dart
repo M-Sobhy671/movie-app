40e4d50dc285824c12f0bf55e43cfa2c2935ae83
Mohamed Sobhy <mo.sobhy.z11@gmail.com>
2026-05-01 05:12:28 +0300

    solve images problem

--- lib/utiles/appAssets.dart
+++ lib/utiles/appAssets.dart
@@ -1,13 +1,13 @@
 class Appassets {
-  static const String forgetPass = 'assets/images/Forget_Password.png';
-  static const String mail_icon = 'assets/images/mail.png';
-  static const String avatar_1 = 'assets/images/avatar_1.png';
-  static const String avatar_2 = 'assets/images/avatar_2.png';
-  static const String avatar_3 = 'assets/images/avatar_3.png';
-  static const String avatar_4 = 'assets/images/avatar_4.png';
-  static const String avatar_5 = 'assets/images/avatar_5.png';
-  static const String avatar_6 = 'assets/images/avatar_6.png';
-  static const String avatar_7 = 'assets/images/avatar_7.png';
-  static const String avatar_8 = 'assets/images/avatar_8.png';
-  static const String avatar_9 = 'assets/images/avatar_9.png';
+  static const String forgetPass = 'assets/temp_images/Forget_Password.png';
+  static const String mail_icon = 'assets/temp_images/mail.png';
+  static const String avatar_1 = 'assets/temp_images/avatar_1.png';
+  static const String avatar_2 = 'assets/temp_images/avatar_2.png';
+  static const String avatar_3 = 'assets/temp_images/avatar_3.png';
+  static const String avatar_4 = 'assets/temp_images/avatar_4.png';
+  static const String avatar_5 = 'assets/temp_images/avatar_5.png';
+  static const String avatar_6 = 'assets/temp_images/avatar_6.png';
+  static const String avatar_7 = 'assets/temp_images/avatar_7.png';
+  static const String avatar_8 = 'assets/temp_images/avatar_8.png';
+  static const String avatar_9 = 'assets/temp_images/avatar_9.png';
 }
