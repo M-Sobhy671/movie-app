1b452edd305ed1d13cad3347b920960bc8b4dd5b
Mohamed Sobhy <mo.sobhy.z11@gmail.com>
2026-05-15 16:55:41 +0300

    handle movieItem

--- lib/widgets/movie_item.dart
+++ lib/widgets/movie_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/utiles/appAssets.dart';
+import 'package:movie_app/utiles/appColors.dart';
 
 class MovieItem extends StatelessWidget {
   final String image;
@@ -14,6 +16,8 @@ class MovieItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return GestureDetector(
       onTap: onTap,
       child: Container(
@@ -21,9 +25,34 @@ class MovieItem extends StatelessWidget {
         width: w(0.35),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(w(0.03)),
-          child: Image.asset(
-            image,
-            fit: BoxFit.cover,
+          child: Stack(
+            children: [Image.asset(
+              image,
+              fit: BoxFit.cover,
+            ),
+              Container(
+                margin: EdgeInsets.all(width*0.02),
+                decoration: BoxDecoration(
+                    color: Color(0xB5121312),
+                    borderRadius: BorderRadiusGeometry.circular(10)
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical:width*0.01
+                      ,horizontal:height*0.01 ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: width*0.02,
+                    children: [
+                      Text('7.6',style: TextStyle(
+                        color: Appcolors.whiteColor,
+                        fontSize:16,
+                      ),),
+                      Image.asset(Appassets.fav_icon),
+                    ],
+                  ),
+                ),
+              )
+          ]
           ),
         ),
       ),
